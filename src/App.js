0372a32640f373afc4002c97a0c9f5355395c8d9
smackflad <ask.kalamakhs@hotmail.com>
2021-12-27 10:52:03 +0200

    learning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import MyButton from "./components/MyButton/MyButton";
+import MyBreadcrumb from "./components/MyBreadcrumb/MyBreadcrumb";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
        return (
-	   <MyButton btn_color="brown"/>
+		   <div>
+			   <MyButton btn_color="brown"/>
+			   <MyBreadcrumb/>
+		   </div>
 	   );
 }
 
