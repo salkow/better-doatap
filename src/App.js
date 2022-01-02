99db796588f906169ac0ed4c2c4033fc848e0a7d
salkow <salkow@outlook.com>
2022-01-02 15:55:39 +0200

    Navbar is now almost done.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Communication from "./components/Pages/myCommunication/myCommunication"
 import QNA from "./components/Pages/myQNA/myQNA"
 import MyLoginPage from "./components/Pages/myLoginPage/MyLoginPage";
 
-import Nav from "./components/generalComponents/myNav/myNav"
+import TheNav from "./components/generalComponents/myNav/myNav"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -12,7 +12,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 function App() {
        return (
 		   <BrowserRouter>
-		   			<Nav/>
+		   			<TheNav/>
 			   <Routes>
 				   <Route path="/" element={<HomePage/>}/> 
 				   <Route path="/new_application" element={<HomePage/>}/> 
--- src/components/generalComponents/myNav/myNav.js
+++ src/components/generalComponents/myNav/myNav.js
@@ -1,99 +1,189 @@
 import { NavLink } from "react-router-dom";
 
-import { Dropdown } from "react-bootstrap";
+import logo from "../../../doatap-logo.png";
+import user_icon from "../../../user-icon.png";
+import MySearchBar from "../../generalComponents/mySearchBar/mySearchBar";
 
-import logo from "../../../doatap-logo.png"
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
-import "./myNav.css"
+import "./myNav.css";
 
-const Nav = () => {
+const TheNav = () => {
   return (
-    <nav
-      className="navbar navbar-expand-md navbar-dark bg-dark mb-4"
-      // ref={navRef}
-    >
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          <img
-	  		src={logo}
-            alt="Logo"
-            width="35"
-            height="35"
-          />
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink
-                exact
-                activeClassName="active"
-                className="nav-link"
-                to="/new_application"
-              >
-                Κάνε αίτηση
-              </NavLink>
-            </li>
-	  
-            <li className="nav-item">
-              <NavLink
-                exact
-                activeClassName="active"
-                className="nav-link"
-                to="/how_to_create_new_application"
-              >
-                Πώς να κάνω αίτηση
-              </NavLink>
-            </li>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo} alt="doatap logo" width="35" height="35" />
+          </NavLink>
+        </Navbar.Brand>
 
-            <li className="nav-item">
-              <Dropdown style={{marginTop: "2%"}}>
-                <Dropdown.Toggle variant="dark" id="dropdown-basic" style={{color: "darkgray", boxShadow: "none", border: "none", backgroundColor: "#212529"}}>
-                  Πληροφορίες
-                </Dropdown.Toggle>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink
+              exact
+              activeClassName="active"
+              className="nav-link"
+              to="/new_application"
+            >
+              Κάνε αίτηση
+            </NavLink>
 
-                <Dropdown.Menu variant="dark">
+            <NavLink
+              exact
+              activeClassName="active"
+              className="nav-link"
+              to="/how_to_create_new_application"
+            >
+              Πώς να κάνω αίτηση
+            </NavLink>
 
-                  <Dropdown.Item>
-                    <NavLink
-                      exact
-                      activeClassName="active"
-                      className="nav-link"
-                      to="/communication"
-                    >
-                      Επικοινωνία
-                    </NavLink>
-                  </Dropdown.Item>
-	  
-                  <Dropdown.Item>
-                    <NavLink
-                      exact
-                      activeClassName="active"
-                      className="nav-link"
-                      to="/qna"
-                    >
-                      Συχνές ερωτήσεις
-                    </NavLink>
-                  </Dropdown.Item>
+            <NavDropdown title="Πληροφορίες">
+              <NavDropdown.Item>
+                <NavLink
+                  exact
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/communication"
+                >
+                  Επικοινωνία
+                </NavLink>
+              </NavDropdown.Item>
 
-                </Dropdown.Menu>
-              </Dropdown>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+              <NavDropdown.Item>
+                <NavLink
+                  exact
+                  activeClassName="active"
+                  className="nav-link"
+                  to="/qna"
+                >
+                  Συχνές ερωτήσεις
+                </NavLink>
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+
+          <MySearchBar/> 
+
+          <Nav>
+            <NavLink
+              exact
+              activeClassName="active"
+              className="nav-link"
+              to="/new_application"
+            >
+              Είσοδος
+            </NavLink>
+
+            <NavLink
+              exact
+              activeClassName="active"
+              className="nav-brand"
+              to="/new_application"
+            >
+              <img src={user_icon} alt="doatap logo" width="35" height="35" />
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Nav;
+// const Nav = () => {
+//   return (
+//     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+//       <div className="container-fluid">
+//         <NavLink className="navbar-brand" to="/">
+//           <img src={logo} alt="Logo" width="35" height="35" />
+//         </NavLink>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarSupportedContent"
+//           aria-controls="navbarSupportedContent"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item">
+//               <NavLink
+//                 exact
+//                 activeClassName="active"
+//                 className="nav-link"
+//                 to="/new_application"
+//               >
+//                 Κάνε αίτηση
+//               </NavLink>
+//             </li>
+
+//             <li className="nav-item">
+//               <NavLink
+//                 exact
+//                 activeClassName="active"
+//                 className="nav-link"
+//                 to="/how_to_create_new_application"
+//               >
+//                 Πώς να κάνω αίτηση
+//               </NavLink>
+//             </li>
+
+//             <li className="nav-item">
+//               <Dropdown style={{ marginTop: "2%" }}>
+//                 <Dropdown.Toggle
+//                   variant="dark"
+//                   id="dropdown-basic"
+//                   style={{
+//                     color: "darkgray",
+//                     boxShadow: "none",
+//                     border: "none",
+//                     backgroundColor: "#212529",
+//                   }}
+//                 >
+//                   Πληροφορίες
+//                 </Dropdown.Toggle>
+
+//                 <Dropdown.Menu variant="dark">
+//                   <Dropdown.Item>
+//                     <NavLink
+//                       exact
+//                       activeClassName="active"
+//                       className="nav-link"
+//                       to="/communication"
+//                     >
+//                       Επικοινωνία
+//                     </NavLink>
+//                   </Dropdown.Item>
+
+//                   <Dropdown.Item>
+//                     <NavLink
+//                       exact
+//                       activeClassName="active"
+//                       className="nav-link"
+//                       to="/qna"
+//                     >
+//                       Συχνές ερωτήσεις
+//                     </NavLink>
+//                   </Dropdown.Item>
+//                 </Dropdown.Menu>
+//               </Dropdown>
+//             </li>
+
+//             <li id="user-icon">
+//               <NavLink className="navbar-item" to="/">
+//                 <img src={user_icon} alt="Logo" width="35" height="35" />
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+export default TheNav;
