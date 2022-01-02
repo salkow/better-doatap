34c5562e98f0784d5f7b76523f2eecbdcc20c707
smackflad <ask.kalamakhs@hotmail.com>
2022-01-02 18:55:41 +0200

    made searchbar work with flexbox cause we are smart! added 2 comments that are need to changed if want smaller (made it a bit smaller but need to talk about this

--- src/components/generalComponents/mySearchBar/mySearchBar.js
+++ src/components/generalComponents/mySearchBar/mySearchBar.js
@@ -4,10 +4,10 @@ const MySearchBar = ({}) =>
 {
     return (
         <div class="searchBox">
+            <input class="searchInput" type="search" name="q" placeholder="Αναζήτηση..."></input>
             <button class="searchButton" type="submit">
                 <i class="material-icons"> search </i>
             </button>
-            <input class="searchInput" type="search" name="q" placeholder="Αναζήτηση..."></input>
         </div>
 	);
 };
