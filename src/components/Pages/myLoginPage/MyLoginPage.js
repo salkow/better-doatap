d77398ff90fa6b18bea278892873f79475525cbc
smackflad <ask.kalamakhs@hotmail.com>
2022-01-03 21:58:31 +0200

    created breadcrubs check comments

--- src/components/Pages/myLoginPage/MyLoginPage.js
+++ src/components/Pages/myLoginPage/MyLoginPage.js
@@ -4,28 +4,33 @@ import './MyLoginPage.css';
 
 import MyTextBox from "../../generalComponents/MyTextBox/MyTextBox";
 import MyButton from "../../generalComponents/MyButton/MyButton";
+import MyBreadcrumb from '../../generalComponents/MyBreadcrumb/MyBreadcrumb';
 
 // import { useState, useEffect } from "react";
 
 const MyLoginPage = () => 
 {
+    const myBread = [{first: "#", second: "home"}, {first: "#", second: "test"}, {second: "non"}]
     return (
-		<div class="external">
-      <div class="internal">
-        <div class="top">
-          <span id="underlined">Είσοδος/Εγγραφή</span>
-        </div>
-        <div class="middle">
-            <span class="head">Λογαριασμός Σελίδας</span>
-            <div class="txtboxes">
-              <MyTextBox txt="Email Address" type="text"/>
-              <MyTextBox txt="Password" type="password"/>
+      <div>
+        <MyBreadcrumb array={myBread}/>
+        <div class="external">
+          <div class="internal">
+            <div class="top">
+              <span id="underlined">Είσοδος/Εγγραφή</span>
             </div>
-            <MyButton btn_color="#4285f4" txt_color="white" curr_msg="Σύνδεση"/>
-            <span  class="tail">Δεν έχεις λογαριασμό; <span class="tail-colored">Εγγραφή</span></span>
+            <div class="middle">
+                <span class="head">Λογαριασμός Σελίδας</span>
+                <div class="txtboxes">
+                  <MyTextBox txt="Email Address" type="text"/>
+                  <MyTextBox txt="Password" type="password"/>
+                </div>
+                <MyButton btn_color="#4285f4" txt_color="white" curr_msg="Σύνδεση"/>
+                <span  class="tail">Δεν έχεις λογαριασμό; <span class="tail-colored">Εγγραφή</span></span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 	);
 };
 
