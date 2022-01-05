ac8eb80c588af08d0ff4a5f8e0d91541cc1d50be
smackflad <ask.kalamakhs@hotmail.com>
2022-01-05 22:06:57 +0200

    fixed myapplication icon size

--- src/components/Pages/myHomePage/myHomePage.js
+++ src/components/Pages/myHomePage/myHomePage.js
@@ -2,17 +2,7 @@ import MyBreadcrumb from "../../generalComponents/MyBreadcrumb/MyBreadcrumb";
 
 const HomePage = () => {
   return (
-    <div className="content">
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
-      <h1>This is the home page</h1>
+    <div>
       <h1>This is the home page</h1>
       <h1>This is the home page</h1>
       <h1>This is the home page</h1>
