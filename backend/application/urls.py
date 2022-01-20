50cdd712a35817d0aa2395c78563b7f5d1fda71b
salkow <salkow@outlook.com>
2022-01-20 16:30:25 +0200

    Added create of application without user id.

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -3,6 +3,7 @@ from .views import ApplicationList
 from .views import ApplicationDetail
 from .views import ApplicationListAdmin
 from .views import ApplicationDetailAdmin
+from .views import ApplicationList_1
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,6 +13,7 @@ from rest_framework_simplejwt.views import (
 app_name = 'application'
 
 urlpatterns = [
+    path('x/', ApplicationList_1.as_view(), name='application_list_1'),
     path('', ApplicationList.as_view(), name='application_list'),
     path('admin/', ApplicationListAdmin.as_view(), name='application_list_admin'),
     path('admin/<int:pk>/', ApplicationDetailAdmin.as_view(), name='application_list_admin'),
